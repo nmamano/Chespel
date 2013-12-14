2769a1f328b49e29df4e6271ff9885678a6f1f33
Nil Mamano <tehotserver@gmail.com>
2013-12-14 20:05:54 +0100

    parser: read_input(), stores the input in a vector of chars

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,30 +1,55 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <stdio.h>
 
 using namespace std;
 
-struct AbstractSyntaxTree {
-	node* root;
-};
-
-struct node {
+struct Node {
 	string type;
 	string content;
-	node* father;
-	vector<node*> sons;
+	Node* father;
+	vector<Node*> sons;
 };
+
+struct AbstractSyntaxTree {
+	Node* root;
+};
+
+vector<char> read_input() {
+	vector<char> input(0);
+	
+	bool eof = false;
+	while (not eof) {
+		int c;
+		c = getchar();
+		if (c == -1) eof = true;
+		else input.push_back((char) c);
+	}
+
+	return input;
+}
+
+void test_read_input(const vector<char>& input) {
+	for (unsigned int i = 0; i < input.size(); ++i) {
+		cout << input[i];
+	}
+	cout << endl;
+}
 
 int main() {
 
 	vector<char> input;
 	input = read_input();
 	
-	vector<string> token_stream;
-	token_stream = lexical_analysis(input);
-	
-	AbstractSyntaxTree ast;
-	ast = syntax_analysis(token_stream);
+	test_read_input(input);
 
-	semantic_analysis(ast);
+	//vector<string> token_stream;
+	//token_stream = lexical_analysis(input);
+	
+	//AbstractSyntaxTree ast;
+	//ast = syntax_analysis(token_stream);
+
+	//semantic_analysis(ast);
 }
+
