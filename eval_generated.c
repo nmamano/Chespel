4a301443a37d9f3770185918ceb8ba6e75d2aad2
David Duran Rosich <david.duran.rosich@est.fib.upc.edu>
2014-05-27 22:24:25 +0200

    Deleted tracking eval_generated.c

--- eval_generated.c
+++
@@ -1,9 +0,0 @@
-#include <string.h>
-#include "faile.h"
-#include "extvars.h"
-#include "protos.h"
-
-// Configs
-extern const int centipawn_value = 20000;
-extern const bool default_PStables = FALSE;
-
